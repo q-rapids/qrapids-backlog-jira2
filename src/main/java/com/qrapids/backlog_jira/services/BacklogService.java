64b9b3bfc99962d362062b02522171966c452cb2
Ariadna Viñets <37736097+arivc@users.noreply.github.com>
2021-03-09 18:25:11 +0100

    Issues generados correctamente
    post correcto, no salta excepcion

--- src/main/java/com/qrapids/backlog_jira/services/BacklogService.java
+++ src/main/java/com/qrapids/backlog_jira/services/BacklogService.java
@@ -1,7 +1,11 @@
 package com.qrapids.backlog_jira.services;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.qrapids.backlog_jira.data.ErrorResponse;
@@ -11,7 +15,6 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.Base64;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BacklogService {
 
     @PostMapping("/api/createIssue")
-    public ResponseEntity<Object> createIssue(@RequestBody QualityRequirement requirement) {
+    public ResponseEntity<Object> createIssue(@RequestBody QualityRequirement requirement) throws IOException {
         try {
+            SuccessResponse newIssue = null;
             //Creating the request authentication by username and apiKey
             ClientResponse response;
             String auth = new String(Base64.encode("ariadna.vinets@estudiantat.upc.edu" + ":" + "BaDD9uM0B6VtdOWuwRkQ3C2B"));
@@ -37,10 +41,10 @@ public class BacklogService {
             String data1 = "{\"fields\":{\"project\":{\"key\":\"TP\"},\"summary\":\"REST Test\",\"description\": \"Creating of an issue using project keys and issue type names using the REST API\",\"issuetype\":{\"name\":\"Bug\"}}}";
 
             //Creating all the data with the requirement
-            String data = "{\"fields\":{\"project\":{\"key\":\"" +requirement.getProject_id() + "\""
-                    +"},\"summary\":\""+ requirement.getIssue_summary() + "\""
-                    +",\"description\":\" " + requirement.getIssue_description()+ "\"" + ",\"issuetype\":{\"name\":\""
-                    + requirement.getIssue_type() +"\""+ "}}}";
+            String data = "{\"fields\":{\"project\":{\"key\":\"" + requirement.getProject_id() + "\""
+                    + "},\"summary\":\"" + requirement.getIssue_summary() + "\""
+                    + ",\"description\":\" " + requirement.getIssue_description() + "\"" + ",\"issuetype\":{\"name\":\""
+                    + requirement.getIssue_type() + "\"" + "}}}";
 
             //POST METHOD JIRA
             WebResource webResource = client.resource("https://ariadnavinets.atlassian.net/rest/api/2/issue");
@@ -54,29 +58,29 @@ public class BacklogService {
                         + statusCode);
 
             } else {
-
-                try (BufferedReader br = new BufferedReader(
-                        new InputStreamReader(response.getEntityInputStream(), "utf-8"))) {
+                try
+                        (BufferedReader br = new BufferedReader(
+                                new InputStreamReader(response.getEntityInputStream(), StandardCharsets.UTF_8))) {
                     StringBuilder resp = new StringBuilder();
-                    String responseLine = null;
+                    String responseLine;
                     while ((responseLine = br.readLine()) != null) {
                         resp.append(responseLine.trim());
                     }
-                    //System.out.println(resp.toString());
+                    System.out.println(resp.toString());
 
                     JsonParser parser = new JsonParser();
                     JsonObject object = parser.parse(resp.toString()).getAsJsonObject();
+
                     System.out.println(object);
-                    SuccessResponse newIssue = new SuccessResponse(object.get("iid").getAsString(), object.get("web_url").getAsString());
+                    newIssue = new SuccessResponse(object.get("id").getAsString(), object.get("self").getAsString());
                     return new ResponseEntity<>(newIssue, HttpStatus.OK);
                 }
             }
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        catch (Exception e) {
-                return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
     }
+}
+
 
 
